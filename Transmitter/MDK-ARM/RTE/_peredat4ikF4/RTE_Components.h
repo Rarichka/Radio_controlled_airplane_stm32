0de59f6a20d36e86228696d8e2517e45e571b0fe
Rari <gjdjnfhfgnd@gmail.com>
2025-09-06 23:29:54 +0500

    Plane

---
+++ Transmitter/MDK-ARM/RTE/_peredat4ikF4/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'peredat4ikF4' 
+ * Target:  'peredat4ikF4' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
